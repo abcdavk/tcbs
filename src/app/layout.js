3db67fc84f4e4209b100356a9f52aa8f113aa7c4
abcdavk <xloot.mcpedl@gmail.com>
2023-12-04 04:35:37 +0700

    Update App

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from 'next/font/google'
+import Head from 'next/head';
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -11,6 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <script type='text/javascript' src='//pl21636205.toprevenuegate.com/de/4b/06/de4b06433e4200dfcfe3082972bcee87.js'></script>
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   )
